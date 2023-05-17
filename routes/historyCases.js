a8968d3cd3d566e72ccf92aba25a49dbe72af6f7
Mathieu Cauquil <mathieu.cauquil@bleu122.com>
2023-05-17 16:00:41 +0200

    commit

--- routes/historyCases.js
+++ routes/historyCases.js
@@ -1,13 +1,21 @@
 const express = require("express");
 const router = express.Router();
+const HistoryCases = require("../models/historyCase");
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
     console.log("Just got a GET requst!")
-    res.send([
+    try {
+        const cases = await HistoryCases.find();
+        res.json(cases);
+    }
+    catch(err){
+        res.send({message: err.message})
+    }
+    /*res.send([
         {age: 40, first_name: 'Dickerson', last_name: 'Macdonald'},
         {age: 15, first_name: 'Shadow', last_name: 'Poupoune'},
         {age: 22, first_name: 'Louis', last_name: 'XVI'}
-    ])
+    ])*/
 })
 
 
