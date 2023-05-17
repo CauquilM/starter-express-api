31b76dc372558d406ab7d7e5c07f2dd2b89d0b00
Mathieu Cauquil <mathieu.cauquil@bleu122.com>
2023-05-17 14:24:35 +0200

    commit

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+const mongoose = require("mongoose");
+require('dotenv').config();
+
+
+mongoose.set('strictQuery', true);
+
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
+app.get('/', (req, res) => {
+    console.log("Just got a GET requst!")
+    res.send({
+        name: "Doudou", type: "criminal", charge: "love"
+    });
+})
 app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo Mas!')
+    console.log("Just got a POST request!")
+    res.send('Yo Mas, I received your dat!')
 })
 app.listen(process.env.PORT || 3000)
